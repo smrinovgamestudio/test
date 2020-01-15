794a2c5176fca08ead624c0ac4b7bc358ffaac96
Mikhail Smirnov <mikhailsmirnov@MacBook-Pro-User.local>
2020-01-15 14:46:42 +0300

    Updates

---
+++ precache-manifest.95de50125bf95c2336d6069f708b82a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d906feb0b4558e57d22eb511f67f293",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6909536d36fc1acf6b62",
+    "url": "./static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "5f087740dbbfca9d665d",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "6909536d36fc1acf6b62",
+    "url": "./static/js/2.00b4aefb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.00b4aefb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f087740dbbfca9d665d",
+    "url": "./static/js/main.6e66fa64.chunk.js"
+  },
+  {
+    "revision": "a2b093908b6b1ddd9337",
+    "url": "./static/js/runtime-main.4cee0d13.js"
+  },
+  {
+    "revision": "1aa0c0c53ebd6928c2e5768b343be42f",
+    "url": "./static/media/cpabro.1aa0c0c5.png"
+  }
+]);
